af5eefef4913f194d06d4465c0a2a9b291654173
Sigeno <sigenogoro@users.noreply.github.com>
2019-10-06 22:25:48 +0900

    display graphs with single click

--- graph.js
+++ graph.js
@@ -49,28 +49,32 @@
         },
     ]
 
-    var cy = cytoscape({
-        container: document.getElementById('cy'),
-        // エッジとノードを指定
-        elements: sample_data ,
-        style: sample_style,
-    });
+    class Cygraph {
+        display_graph(){
+            const cy = cytoscape({
+                container: document.getElementById('cy'),
+                // エッジとノードを指定
+                elements: sample_data ,
+                style: sample_style,
+            });
+            const layout = cy.elements().layout(
+                {
+                    name: 'grid',
+                    columns: 2
+                }
+            );
+           layout.run();
+        }
+    }
 
     // バージョン変更の影響を受けている 3.2.12以降
-    var layout = cy.elements().layout(
-        {
-            name: 'grid',
-            columns: 2
-        }
-    );
+    
 
-
-    layout.run();
-
+    
+    const cygraph = new Cygraph();
     const btn = document.getElementById('btn');
-    console.log(btn);
     btn.addEventListener('click', () => {
-        console.log('OK');
+        cygraph.display_graph();
     });
 
 }
